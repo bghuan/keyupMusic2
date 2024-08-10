4be8d67ebaebb8fdf1e78e4f8df0cf1805bc2cf3
bghuan <132>
2024-08-10 10:00:55 +0800

    加些快捷键

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,31 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Huan));
             label1 = new Label();
+            notifyIcon1 = new NotifyIcon(components);
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(6, -4);
             label1.Name = "label1";
             label1.Size = new Size(53, 20);
             label1.TabIndex = 1;
             label1.Text = "label1";
             label1.Click += label1_Click;
             // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "幻";
+            notifyIcon1.Visible = true;
+            notifyIcon1.Click += notifyIcon1_Click;
+            // 
             // Huan
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(275, 42);
+            ClientSize = new Size(269, 22);
             Controls.Add(label1);
             Location = new Point(2270, 100);
             Name = "Huan";
+            ShowInTaskbar = false;
             StartPosition = FormStartPosition.Manual;
             Text = "幻";
             TopMost = true;
             Load += Form1_Load;
+            DoubleClick += Huan_ResizeEnd;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -60,5 +72,6 @@
         #endregion
 
         private Label label1;
+        private NotifyIcon notifyIcon1;
     }
 }
---
+++ winBinWallpaper.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace keyupMusic2
+{
+    public class winBinWallpaper
+    {
+        static string picUrl = "https://bghuan.cn/api/bing.php";
+        static bool changing = false;
+        static void Main1111(string[] args)
+        {
+            changeImg();
+        }
+
+        public static void changeImg()
+        {
+            string savePath = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMdd") + ".jpg";
+            if (File.Exists(savePath)) return;
+            if (changing) return;
+
+            changing = true;
+            System.Net.ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
+            string value = String.Empty;
+            WebResponse response = null;
+            Stream stream = null;
+            try
+            {
+                WebClient mywebclient = new WebClient();
+                mywebclient.DownloadFile(picUrl, savePath);
+                value = savePath;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (stream != null) stream.Close();
+                if (response != null) response.Close();
+                changing = false;
+            }
+
+
+            int nResult;
+            if (File.Exists(value))
+            {
+
+                nResult = WinAPI.SystemParametersInfo(20, 1, value, 0x1 | 0x2); //更换壁纸
+                if (nResult == 0)
+                {
+                    Console.WriteLine("err");
+                }
+                else
+                {
+                    RegistryKey hk = Registry.CurrentUser;
+                    RegistryKey run = hk.CreateSubKey(@"Control Panel\Desktop\");
+                    run.SetValue("Wallpaper", value);  //将新图片路径写入注册表
+                    Console.WriteLine("success");
+                }
+            }
+            else
+            {
+                Console.WriteLine("not exist");
+            }
+            changing = false;
+        }
+
+        public class WinAPI
+        {
+            [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+            public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        }
+    }
+}
